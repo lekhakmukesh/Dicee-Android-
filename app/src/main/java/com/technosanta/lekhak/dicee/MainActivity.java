812a3631b52efb71390a45da478f938cf34e5fa2
Mukesh Lekhak <lekhak_mukesh@live.com>
2017-11-23 08:06:33 +0545

    App completed and works fine in emulator

--- app/src/main/java/com/technosanta/lekhak/dicee/MainActivity.java
+++ app/src/main/java/com/technosanta/lekhak/dicee/MainActivity.java
@@ -2,12 +2,53 @@ package com.technosanta.lekhak.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button rollButton;
+    ImageView dice1;
+    ImageView dice2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rollButton = findViewById(R.id.rollButton);
+
+        final int diceArray[] = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee", "Random number is " + number);
+                dice1.setImageResource(diceArray[number]);
+
+                number = randomNumberGenerator.nextInt(6);
+                dice2.setImageResource(diceArray[number]);
+
+            }
+        });
+
+        dice1 = findViewById(R.id.dice1);
+        dice2 = findViewById(R.id.dice2);
+
+
+
     }
 }
